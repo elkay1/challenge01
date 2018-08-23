4df55bba3af88e2802707e761cb0479e58e702ef
Liam Kinson <kinsonclarinet@gmail.com>
2018-08-23 14:09:05 +1000

    part 2 done

--- c01.js
+++ c01.js
@@ -1,18 +1,44 @@
 var X = "<div class='cross1'></div><div class='cross2'></div>";
-let playerOne = $("#playerOneName").val();
-
+var jet = "<i class='fa fa-fighter-jet'>";
+var bullseye = "<i class='fa fa-bullseye'></i>";
 $(document).ready(function(){
+
 $("#startGame").on("click", function(){
    DrawBoard(); 
 });
 
+    
+$(".board").on("click", "td", function () {
 
-
-
+    if ($(this).html(bullseye)){
+        $(this).html(jet);
+    };
+    });    
+$("#restartGame").on("click", function(){
+    ClearBoard();
+})
 
 });
+
+
+
+//    if ((this).innerHTML == ) {
+//        $(this).addClass("selected");
+//        if (playerOneTurn == true) {
+//            $("#scoreone").html(playerOneScore);
+//            playerOneScore++;
+//            console.log(playerOneScore);
+//        } else {
+//            $("#scoretwo").html(playerTwoScore);
+//            playerTwoScore++;
+//            console.log(playerTwoScore);
+//        }
+
+//    } 
+
 function DrawBoard(){
 let mytable = "<table class='mytable'><tbody>";
+let playerOne = $("#playerOneName").val();
 
 
     $("#playerone").html("<p>" + playerOne + "'s Score: </p>");
@@ -22,9 +48,25 @@ let mytable = "<table class='mytable'><tbody>";
     for (var i = 1; i < 11; i++) {
         mytable += "<tr>";
         for (var j = 1; j < 11; j++) {
-            mytable += "<td>" + "<i class='fa fa-bullseye'></i>" + "</td>";
+            mytable += "<td>" + bullseye + "</td>";
         }
     }
     mytable += "</tbody></table>";
     $(".board").html(mytable);
 };
+
+function ClearBoard(){
+    let mytable = "<table class='mytable'><tbody>";
+
+    $("#playerone").html("");
+    $("#aiScore").html("");
+    $("#recent").html("");
+        for (var i = 1; i < 11; i++) {
+        mytable += "<tr>";
+        for (var j = 1; j < 11; j++) {
+            mytable += "<td>" + bullseye + "</td>";
+        }
+    }
+    mytable += "</tbody></table>";
+    $(".board").html(mytable);
+}
