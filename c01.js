431905167ab1a82b267fa6d35aa6319e9e66a11f
Liam Kinson <kinsonclarinet@gmail.com>
2018-08-23 16:44:17 +1000

    working prototype

--- c01.js
+++ c01.js
@@ -1,58 +1,93 @@
-var X = "<div class='cross1'></div><div class='cross2'></div>";
+
+
+$(document).ready(function(){
+var X = "<i class='fa fa-times'>";
 var jet = "<i class='fa fa-fighter-jet'>";
 var bullseye = "<i class='fa fa-bullseye'></i>";
-$(document).ready(function(){
+var column_num;
+var row_num; 
+var coordArr = [];
+let guess;
+let id = 1;
+let aiScore;
+let playerOneScore;
+let playerOne = $("#playerOneName").val();
+    
 
+   
+    
+    
+    
 $("#startGame").on("click", function(){
    DrawBoard(); 
 });
 
     
-$(".board").on("click", "td", function () {
+    $(".board").on("click", "td", function () {
 
-    if ($(this).html(bullseye)){
+//    row_num = parseInt($(this).parent().index()) + 1;
+//    column_num = parseInt($(this).index()) + 1;
+
+    coordArr.push(parseInt($(this).attr("id")));
+    //
+    //
+    //    console.log(coordArr);
+
+    if ($(this).html(bullseye)) {
         $(this).html(jet);
     };
-    });    
+    console.log(coordArr);
+});   
+    
+    
+    
+    
 $("#restartGame").on("click", function(){
     ClearBoard();
 })
 
+
+
+$("#fightMe").on("click", function () {
+    guess = Math.floor((Math.random() * 100) + 1);
+    //console.log(guess);
+    for (var i = 1; i < coordArr.length; i++) {
+        //        $("#recentRow").html(`Recent Row: ${row_num}`);       $("#recentCol").html(`Recent Column: ${column_num}`);
+        if (guess === coordArr[i]) {
+            console.log(coordArr[i]);
+            $(`#${coordArr[i]}`).html(X);
+            aiScore += 10;
+            $("#aiScore").html("<p> AI's Score: </p>" + aiScore);
+            break;
+        } else {
+            playerOneScore++;
+            $("#playerone").html("<p>" + playerOne + "'s Score: </p>" + playerOneScore);
+            break;
+        }
+    }
+    console.log(playerOneScore);
+    $("#tdRolled").html(`The number rolled is ${guess}`);
 });
-
-
-
-//    if ((this).innerHTML == ) {
-//        $(this).addClass("selected");
-//        if (playerOneTurn == true) {
-//            $("#scoreone").html(playerOneScore);
-//            playerOneScore++;
-//            console.log(playerOneScore);
-//        } else {
-//            $("#scoretwo").html(playerTwoScore);
-//            playerTwoScore++;
-//            console.log(playerTwoScore);
-//        }
-
-//    } 
-
+    
 function DrawBoard(){
 let mytable = "<table class='mytable'><tbody>";
-let playerOne = $("#playerOneName").val();
-
-
-    $("#playerone").html("<p>" + playerOne + "'s Score: </p>");
+aiScore = 0;
+playerOneScore = 0;
+    $("#playerone").html("<p>" + playerOne + "'s Score: </p>" + playerOneScore);
     console.log(playerOne);
-    $("#aiScore").html("<p> AI's Score: </p>");
+    $("#aiScore").html("<p> AI's Score: </p>" + aiScore);
 
     for (var i = 1; i < 11; i++) {
         mytable += "<tr>";
+        
         for (var j = 1; j < 11; j++) {
-            mytable += "<td>" + bullseye + "</td>";
+            mytable += "<td id=" + id + ">" + bullseye + "</td>";
+            id++;
         }
     }
     mytable += "</tbody></table>";
     $(".board").html(mytable);
+
 };
 
 function ClearBoard(){
@@ -61,6 +96,8 @@ function ClearBoard(){
     $("#playerone").html("");
     $("#aiScore").html("");
     $("#recent").html("");
+    $("#tdRolled").html("");
+    coordArr = [];
         for (var i = 1; i < 11; i++) {
         mytable += "<tr>";
         for (var j = 1; j < 11; j++) {
@@ -69,4 +106,8 @@ function ClearBoard(){
     }
     mytable += "</tbody></table>";
     $(".board").html(mytable);
-}
+    
+}; 
+});
+
+
